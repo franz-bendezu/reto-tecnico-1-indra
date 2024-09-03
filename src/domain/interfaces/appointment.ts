783d72142841b3419d3e0c97e234f12955cbbfc9
Franz Bendezu <franzant20@gmail.com>
2024-09-02 23:45:08 -0500

    feat: add lastStatus property to IAppointment interface

--- src/domain/interfaces/appointment.ts
+++ src/domain/interfaces/appointment.ts
@@ -3,13 +3,15 @@ import {
   IAppointmentStatus,
 } from "./appointment-status";
 
-export interface IAppointment {
+export interface IBaseAppointment {
   insuredId: string;
   scheduleId: number;
   countryISO: string;
+  lastStatus: AppointmentStatusTypes;
+}
+
+export interface IAppointment extends IBaseAppointment {
   createdAt: string; // ISO 8601 date string
   updatedAt: string; // ISO 8601 date string
-  notes?: string;
-  lastStatus: AppointmentStatusTypes;
   statuses: IAppointmentStatus[];
 }
