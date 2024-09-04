fe4be156682892ccd6a5e248b9bdfd9a6a607c25
Franz Bendezu <franzant20@gmail.com>
2024-09-03 22:53:18 -0500

    refactor: add appointmentCLHandler and appointmentPEHandler to use zod for appointment validation

--- src/adapters/handlers/appointmentCLHandler.ts
+++ src/adapters/handlers/appointmentCLHandler.ts
@@ -1,5 +1,30 @@
-import { SQSHandler } from 'aws-lambda';
+import { SQSHandler } from "aws-lambda";
+import { z } from "zod";
+import { appointmentCreateSchema } from "../schemas/appointment";
+import { AppointmentCLService } from "../../domain/services/AppointmentCLService";
+import { AppointmentCountryProducer } from "../../infraestructure/messasing/AppointmentCountryProducer";
+import { AppointmentRDSRepository } from "../../infraestructure/repositories/AppointmentRDSRepository";
+
+const appointmentProducer = new AppointmentCountryProducer();
+const appointmentCountryRepository = new AppointmentRDSRepository();
+const appointmentService = new AppointmentCLService(
+  appointmentCountryRepository,
+  appointmentProducer
+);
 
 export const handler: SQSHandler = async (event) => {
-
+  for (const record of event.Records) {
+    try {
+      const appointment = appointmentCreateSchema.parse(
+        JSON.parse(record.body)
+      );
+      await appointmentService.createAppointment(appointment);
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        console.error("Invalid appointment data", error.errors);
+      } else {
+        console.error("Error processing appointment", error);
+      }
+    }
+  }
 };
--- src/adapters/handlers/appointmentPEHandler.ts
+++ src/adapters/handlers/appointmentPEHandler.ts
@@ -1,5 +1,30 @@
-import { SQSHandler } from 'aws-lambda';
+import { SQSHandler } from "aws-lambda";
+import { z } from "zod";
+import { appointmentCreateSchema } from "../schemas/appointment";
+import { AppointmentCountryProducer } from "../../infraestructure/messasing/AppointmentCountryProducer";
+import { AppointmentCountryRDSRepository } from "../../infraestructure/repositories/AppointmentRDSRepository";
+import { AppointmentPEService } from "../../domain/services/AppointmentPEService";
+
+const appointmentProducer = new AppointmentCountryProducer();
+const appointmentCountryRepository = new AppointmentCountryRDSRepository();
+const appointmentService = new AppointmentPEService(
+  appointmentCountryRepository,
+  appointmentProducer
+);
 
 export const handler: SQSHandler = async (event) => {
-
+  for (const record of event.Records) {
+    try {
+      const appointment = appointmentCreateSchema.parse(
+        JSON.parse(record.body)
+      );
+      await appointmentService.createAppointment(appointment);
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        console.error("Invalid appointment data", error.errors);
+      } else {
+        console.error("Error processing appointment", error);
+      }
+    }
+  }
 };
