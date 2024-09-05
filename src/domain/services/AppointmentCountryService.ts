7800c06bb6c9c08a2956e97257e71d059ca4b728
Franz Bendezu <franzant20@gmail.com>
2024-09-05 04:48:42 -0500

    refactor: set AppointmentCountryService to use IAppointmentProducer for sending appointment events

--- src/domain/services/AppointmentCountryService.ts
+++ src/domain/services/AppointmentCountryService.ts
@@ -1,11 +1,11 @@
-import { IAppointmentCountryProducer } from "../../infraestructure/messasing/IAppointmentCountryProducer";
+import { IAppointmentProducer } from "../../infraestructure/messasing/IAppointmentProducer";
 import { IAppointmentCountryRepository } from "../../infraestructure/repositories/IAppointmentCountryRepository";
 import { IAppointmentCreate } from "../interfaces/appointment-create";
 
 export abstract class AppointmentCountryService {
   constructor(
     private appointmentCountryRepository: IAppointmentCountryRepository,
-    private appointmentCountryProducer: IAppointmentCountryProducer
+    private appointmentCountryProducer: IAppointmentProducer
   ) {}
   async createAppointment(appointment: IAppointmentCreate): Promise<void> {
     await this.appointmentCountryRepository.create({
