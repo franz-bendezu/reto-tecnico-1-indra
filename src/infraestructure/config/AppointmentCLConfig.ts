ef6a10efa7d3e9d3ece071a626f7572d4e73be93
Franz Bendezu <franzant20@gmail.com>
2024-09-05 09:44:44 -0500

    refactor: update database configuration for AppointmentCL and AppointmentPE

--- src/infraestructure/config/AppointmentCLConfig.ts
+++ src/infraestructure/config/AppointmentCLConfig.ts
@@ -5,16 +5,16 @@ export class AppointmentCLConfig extends AppointmentCountryConfig {
   get rdsDatabase(): IDatabaseConfig {
     return {
       get proxyHostName(): string {
-        return process.env.DB_PROXY_HOST_NAME!;
+        return process.env.DB_CL_PROXY_HOST_NAME!;
       },
       get port(): number {
-        return parseInt(process.env.DB_PORT!);
+        return parseInt(process.env.DB_CL_PORT!);
       },
       get dbName(): string {
-        return process.env.DB_NAME!;
+        return process.env.DB_CL_NAME!;
       },
       get dbUserName(): string {
-        return process.env.DB_USER_NAME!;
+        return process.env.DB_CL_USER_NAME!;
       },
     };
   }
--- src/infraestructure/config/AppointmentPEConfig.ts
+++ src/infraestructure/config/AppointmentPEConfig.ts
@@ -5,16 +5,16 @@ export class AppointmentPEConfig extends AppointmentCountryConfig {
   get rdsDatabase(): IDatabaseConfig {
     return {
       get proxyHostName(): string {
-        return process.env.DB_PROXY_HOST_NAME!;
+        return process.env.DB_PE_PROXY_HOST_NAME!;
       },
       get port(): number {
-        return parseInt(process.env.DB_PORT!);
+        return parseInt(process.env.DB_PE_PORT!);
       },
       get dbName(): string {
-        return process.env.DB_NAME!;
+        return process.env.DB_PE_NAME!;
       },
       get dbUserName(): string {
-        return process.env.DB_USER_NAME!;
+        return process.env.DB_PE_USER_NAME!;
       },
     };
   }
