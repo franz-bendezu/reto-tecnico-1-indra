4046980fbd2fa124b9a2001ae1235fbfbd2c90bf
Franz Bendezu <franzant20@gmail.com>
2024-09-05 04:50:41 -0500

    refactor: set AppointmentCountryService to use AppointmentStatusType enum for lastStatus

--- src/domain/services/AppointmentCountryService.ts
+++ src/domain/services/AppointmentCountryService.ts
@@ -1,6 +1,7 @@
 import { IAppointmentProducer } from "../../infraestructure/messasing/IAppointmentProducer";
 import { IAppointmentCountryRepository } from "../../infraestructure/repositories/IAppointmentCountryRepository";
 import { IAppointmentCreate } from "../interfaces/appointment-create";
+import { AppointmentStatusType } from "../models/AppointmentStatus";
 
 export abstract class AppointmentCountryService {
   constructor(
@@ -10,7 +11,7 @@ export abstract class AppointmentCountryService {
   async createAppointment(appointment: IAppointmentCreate): Promise<void> {
     await this.appointmentCountryRepository.create({
       ...appointment,
-      lastStatus: "pending",
+      lastStatus: AppointmentStatusType.PENDING,
     });
     await this.appointmentCountryProducer.sendAppointmentCountry(appointment);
   }
